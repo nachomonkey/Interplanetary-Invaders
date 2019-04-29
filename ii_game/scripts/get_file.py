ef06ea1fbf2fc7b3f18e06a3a25a271444a0a138
NachoMonkey <nachomonkey@catbotlabs.com>
2019-04-29 16:17:25 -0600

    Added more windows-friendly stuff

--- ii_game/scripts/get_file.py
+++ ii_game/scripts/get_file.py
@@ -1,20 +1,21 @@
 from pkg_resources import resource_filename
+from ii_game.scripts.utils import fixPath
 import os
 
 DEV_RUN = os.path.abspath(".").endswith("interplanetary-invaders")
 
 def get_file(filename):
     if filename.startswith("data"):
-        DIR = f"{os.environ['HOME']}/.ii_game-data/"
-        if filename in ("data", "data/"):
+        DIR = fixPath(f"{os.environ['HOME']}/.ii_game-data/")
+        if filename in ("data", fixPath("data/")):
             filename = ""
         else:
-            filename = filename.replace("data/", "")
+            filename = filename.replace(fixPath("data/"), "")
         if not os.path.exists(DIR):
             print(f"Building personal data directory:\n{DIR}")
             os.mkdir(DIR)
         return DIR+filename
     else:
         if DEV_RUN:   # Your running this as a devolper
-            return "ii_game/" + filename # Find it in the current directory
+            return fixPath("ii_game/") + filename # Find it in the current directory
         return resource_filename("ii_game", filename) # Find it in the installation
