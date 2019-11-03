443141d5958825a885cff9bb5e680e62c6deaf2c
NachoMonkey <nachomonkey@catbotlabs.com>
2019-11-03 10:44:31 -0700

    Fixed version and README

--- ii_game/__init__.py
+++ ii_game/__init__.py
@@ -3,6 +3,6 @@ import os
 __myname__ = "Interplanetary Invaders"
 __title__ = "Interplanetary Invaders"
 __author__ = "NachoMonkey"
-__version__ = "0.6"
+__version__ = "0.0.6"
 __minimum_version__ = (3, 7)
 __maximum_version = (3, 9)
